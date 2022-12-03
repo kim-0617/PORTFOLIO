24d91f53ff00eb3b415b9f916d101ea0c2d83c7b
kim-0617 <kkk5993@naver.com>
2022-12-04 02:59:42 +0900

    22.12.04
    2:59

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
 import { Header, Main, Footer } from './layouts';
-import gsap from "gsap"
+import gsap from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import ScrollSmoother from 'gsap-trial/ScrollSmoother';
+import { useEffect } from 'react';
 
 function App() {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  gsap.registerPlugin(ScrollTrigger);
 
-  // create the smooth scroller FIRST!
-  let smoother = ScrollSmoother.create({
-    wrapper: '#smooth-wrapper',
-    content: '#smooth-content',
-    smooth: 2,   // seconds it takes to "catch up" to native scroll position
-    effects: true // look for data-speed and data-lag attributes on elements and animate accordingly
-  });
+  // // create the smooth scroller FIRST!
+  // let smoother = ScrollSmoother.create({
+  //   wrapper: '#smooth-wrapper',
+  //   content: '#smooth-content',
+  //   smooth: 2,   // seconds it takes to "catch up" to native scroll position
+  //   effects: true // look for data-speed and data-lag attributes on elements and animate accordingly
+  // });
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.7,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    })
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'section',
+        scrub: true
+      }
+    })
+      .to('section', {
+        stagger: 1,
+        y: 0,
+      })
+  }, []);
 
   return (
-    <div id="smooth-wrapper">
-      <div id="smooth-content">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
