72065eafe779f8545a2ec3b83a1156a5b81a8949
kim-0617 <kkk5993@naver.com>
2022-12-04 14:32:55 +0900

    22.12.04
    14:32

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header, Main, Footer } from './layouts';
 import gsap from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import './assets/js/lenis';
+import Lenis from '@studio-freight/lenis'
 import { useEffect } from 'react';
 
 function App() {
@@ -15,27 +15,53 @@ function App() {
   //   effects: true // look for data-speed and data-lag attributes on elements and animate accordingly
   // });
 
+  // useEffect(() => {
+  //   const lenis = new Lenis({
+  //     duration: 1.7,
+  //     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+  //   })
+  //   function raf(time) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+  //   requestAnimationFrame(raf);
+
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: 'section',
+  //       scrub: true
+  //     }
+  //   })
+  //     .to('section', {
+  //       stagger: 1,
+  //       y: 0,
+  //     })
+  // }, []);
+
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.7,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+      duration: 3.0,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: 'vertical', // vertical, horizontal
+      gestureDirection: 'vertical', // vertical, horizontal, both
+      smooth: true,
+      mouseMultiplier: 2,
+      smoothTouch: false,
+      touchMultiplier: 50,
+      infinite: false,
     })
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: 'section',
-        scrub: true
-      }
-    })
-      .to('section', {
-        stagger: 1,
-        y: 0,
-      })
+    //get scroll value
+    // lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+    //   console.log({ scroll, limit, velocity, direction, progress })
+    // })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
   }, []);
 
   return (
