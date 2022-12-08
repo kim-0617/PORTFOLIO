8604e0b9c1a780651183851c0bd966323a3f0b37
kim-0617 <kkk5993@naver.com>
2022-12-08 12:53:37 +0900

    22.12.08

--- src/App.jsx
+++ src/App.jsx
@@ -40,12 +40,12 @@ function App() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 2.0,
+      duration: 3.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       direction: 'vertical', // vertical, horizontal
       gestureDirection: 'vertical', // vertical, horizontal, both
       smooth: true,
-      mouseMultiplier: 2,
+      mouseMultiplier: 1,
       smoothTouch: false,
       touchMultiplier: 50,
       infinite: false,
