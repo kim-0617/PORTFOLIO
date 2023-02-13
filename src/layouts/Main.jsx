9058175d8f1f0db96b8eb3b50b4782313dc98f18
kim-0617 <kkk5993@naver.com>
2023-02-13 14:03:00 +0900

    main에서 쓸데없는 주석 삭제

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,47 +1,12 @@
 import { Intro, About, Coding, Dummy } from '../sections';
 import React, { useState } from 'react';
 import { ImgLoader } from '../others';
-// import gsap from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
-// import { Loader, VLoader } from '../others';
-
-// gsap.registerPlugin(ScrollTrigger);
 
 function Main() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  // useEffect(() => {
-  //   if (isLoading) return;
-
-  //   ScrollTrigger.create({
-  //     trigger: '#intro',
-  //     start: 'top top',
-  //     pin: true,
-  //     pinSpacing: false,
-  //   });
-
-  //   ScrollTrigger.create({
-  //     trigger: '#about',
-  //     start: 'bottom bottom',
-  //     pin: true,
-  //     pinSpacing: false,
-  //   });
-
-  //   // ScrollTrigger.create({
-  //   //   snap: 0.332,
-  //   // });
-  // }, [isLoading]);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <main id="main">
-      {/* <>
-        <ImgLoader setIsLoading={setIsLoading} />
-        <Intro isLoading={isLoading} />
-        <About />
-        <Dummy />
-        <Coding />
-      </> */}
-
       {isLoading ? (
         <>
           <ImgLoader setIsLoading={setIsLoading} />
