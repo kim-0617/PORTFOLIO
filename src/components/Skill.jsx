69950ab239ae4bde469c39f05408ba0a1348cba1
kim-0617 <kkk5993@naver.com>
2024-01-04 22:08:39 +0900

    modify : 기술스택 멘트수정

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -40,8 +40,9 @@ function Skill() {
         <div className="skill__title">
           코딩에는 확실한 INPUT과 OUTPUT이 있다는게 매력인 것 같습니다. <br />
           그래서 컴퓨터 공학과를 꿈꿨던 것 같습니다. <br />
-          C를 처음 배우게 됐을 때, <br />
-          저는 콘솔에 Hello World가 찍히는 순간을 아직도 잊지 못합니다. <br />
+          C언어를 처음 배우게 됐을 때, <br />
+          저는 콘솔에 Hello World가 찍히는 순간이 아직도 기억이 납니다.
+          <br />
           항상 초심을 잃지 않고 배우는 자세로 임하겠습니다.
         </div>
 
@@ -52,7 +53,6 @@ function Skill() {
               정보처리기사 <br />
               컴퓨터 활용능력 1급 <br />
               웹 디자인 기능사 <br />
-              ITQ OA MASTER <br />
             </p>
           </div>
           {/* 01 */}
@@ -60,35 +60,25 @@ function Skill() {
           <div className="skill__conts__detail">
             <h3 className="skill__conts__detail__title">퍼블리싱</h3>
             <p className="skill__conts__detail__desc">
-              웹 표준에 따라 마크업이 가능하며, CSS의 여러 중요 속성들을 숙지하고 있으며 SCSS도 사용할 수 있습니다
+              웹 표준에 따라 마크업이 가능하며, CSS의 여러 중요 속성들을 숙지하고 있으며 SCSS를 즐겨 사용합니다.
             </p>
           </div>
           {/* 02 */}
 
           <div className="skill__conts__detail">
-            <h3 className="skill__conts__detail__title">스크립트</h3>
+            <h3 className="skill__conts__detail__title">javascript</h3>
             <p className="skill__conts__detail__desc">
-              기본 메서드, 동기, 비동기, 클래스, 함수형 프로그래밍에 대해 이해하고 있습니다. jQuery도 상황에 맞게 사용할
-              수 있습니다.
+              기본 메서드, 동기, 비동기, 이벤트 루프 등 자바스크립트의 기본 개념에대해 숙지하고 있습니다.
             </p>
           </div>
           {/* 03 */}
 
           <div className="skill__conts__detail">
-            <h3 className="skill__conts__detail__title">PHP</h3>
-            <p className="skill__conts__detail__desc">
-              PHP의 기초적인 문법과 SQL문을 활용한 CRUD 작업을 능숙하게 할 수 있습니다. 또한 이러한과정을 통해서
-              백단에서의 작업 흐름도 이해하고 있습니다.
-            </p>
-          </div>
-          {/* 04 */}
-
-          <div className="skill__conts__detail">
             <h3 className="skill__conts__detail__title">
-              REACT JS <br /> VUE JS
+              REACT JS <br /> Nextjs
             </h3>
             <p className="skill__conts__detail__desc">
-              리액트를 활용한 다양한 라이브러리 활용과 클래스형 컴포넌트, 리액트 훅들을 잘 알고 있습니다.
+              React hooks와 함수형 컴포넌트를 사용하여 프론트 개발에 능숙합니다.
             </p>
           </div>
           {/* 05 */}
@@ -96,8 +86,7 @@ function Skill() {
           <div className="skill__conts__detail">
             <h3 className="skill__conts__detail__title">TYPESCRIPT</h3>
             <p className="skill__conts__detail__desc">
-              기본적인 타이핑과 타입정의, 인터페이스, 제네릭을 학습하였고 이를 리액트 프로젝트에 적용시켜
-              코딩하였습니다.
+              기본적인 타이핑과 타입정의, 인터페이스를 통해 컴파일 단계에서 오류를 방지할 수 있습니다.
             </p>
           </div>
           {/* 06 */}
